3df163cbbcf3bf3b78c1fc10d5e615c4b8507101
Arturo Loya <41841053+ArturShadow@users.noreply.github.com>
2023-02-10 16:26:03 -0600

    Documentacion practica guiada 1

--- WebApp1/Controllers/ClienteController.cs
+++ WebApp1/Controllers/ClienteController.cs
@@ -14,24 +14,26 @@ namespace WebApp1.Controllers
     {
         public IActionResult Index()
         {
-            List<ClienteCLS> listaCliente = new List<ClienteCLS>();
+            //Creamos una lista para guardar la consulta para mostrar en la vista despues
+            List<ClienteCLS>? listaCliente = null;
             //using (var bd = new Models.DB.Tid81dContext())
-            using (var bd = new Models.SQLServer.Tid81dContext())
+            using (var bd = new Models.SQLServer.Tid81dContext()) //Creamos una instancia del contexto de la base de datos
             {
 
                 listaCliente = (from c in bd.Clientes
-                select new ClienteCLS
+                select new ClienteCLS // Se crea un nuevo objeto de la Clase ClientesCLS para cada resultado de la resultado despues lo guarda en la lista
                 {
-                    IdCliente=c.IdCliente,
-                    Nombre=c.Nombre,
-                    APaterno=c.APaterno,
-                    AMaterno=c.AMaterno,
-                    Email=c.Email,
-                    Direccion=c.Direccion,
-                    // Sexo=c.Sexo,
-                    Telefono=c.Telefono
+                    IdCliente=c.IdCliente, //Cada campo del resultado se guarda en su respectivo campo del objeto
+                    Nombre = c.Nombre,
+                    APaterno = c.APaterno,
+                    AMaterno = c.AMaterno,
+                    Email = c.Email,
+                    Direccion = c.Direccion,
+                    // Sexo = c.Sexo,
+                    Telefono = c.Telefono
                 }).ToList();
             }
+            // Se mandas la lista a la vistaz
             return View(listaCliente);
         }
 
--- WebApp1/Models/ClienteCLS.cs
+++ WebApp1/Models/ClienteCLS.cs
@@ -1,45 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
 namespace WebApp1.Models
 {
-    public class ClienteCLS
+    public class ClienteCLS //Clase cliente que es la representacion de la tabla Clientes en la base de datos
     {
-        [Display (Name ="Id Cliente")]
-        [Required]
+        [Display(Name = "Id Cliente")] // Nombre que se va a visualizar al momento de mostrar los datos
+        [Required] //Hace que el campo sea requerido al momento de insrtar un nuevo campo en la base de datos
         public int IdCliente { get; set; }
         [Required]
         [Display(Name = "Nombre cliente")]
-        [StringLength(50,ErrorMessage ="No puede escribir mas de 50 caracteres")]
-        public string Nombre { get; set; }
+        [StringLength(50, ErrorMessage = "No puede escribir mas de 50 caracteres")] //Limita el numero de cvarecetes permitos, sino manda un error
+        public string? Nombre { get; set; }
         [Display(Name = "Apellido Paterno")]
         [Required]
-        [StringLength(50,ErrorMessage ="No puede escribir mas de 50 caracteres")]
-        public string APaterno { get; set; }
+        [StringLength(50, ErrorMessage = "No puede escribir mas de 50 caracteres")]
+        public string? APaterno { get; set; } //Nota: el simbolo de '?' despues del tipo hace que el campo pueda ser nulo
 
         [Display(Name = "Apellido Materno")]
         [Required]
-        [StringLength(50,ErrorMessage ="No puede escribir mas de 50 caracteres")]
-        public string AMaterno { get; set; }
+        [StringLength(50, ErrorMessage = "No puede escribir mas de 50 caracteres")]
+        public string? AMaterno { get; set; }
 
-        
+
         [Display(Name = "Email")]
         [Required]
-        [StringLength(100,ErrorMessage ="No puede escribir mas de 100 caracteres")]
-        public string Email { get; set; }
+        [StringLength(100, ErrorMessage = "No puede escribir mas de 100 caracteres")]
+        public string? Email { get; set; }
         [Display(Name = "Correo")]
         [Required]
-        [StringLength(100,ErrorMessage ="No puede escribir mas de 100 caracteres")]
-        public string Direccion { get; set; }
+        [StringLength(100, ErrorMessage = "No puede escribir mas de 100 caracteres")]
+        public string? Direccion { get; set; }
         // [Display(Name = "Genero")]
         // [Required]
         //  public int Sexo { get; set; }
 
-        [Display(Name="Telefono del cliente")]
+        [Display(Name = "Telefono del cliente")]
         [Required]
-        public string Telefono { get; set; }
+        public string? Telefono { get; set; }
     }
 }
