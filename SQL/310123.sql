01cb86c8ae84b39ea2e7f116b89a20822ecd51f1
Arturo Loya <41841053+ArturShadow@users.noreply.github.com>
2023-03-03 17:17:07 -0600

    Database

--- SQL/310123.sql
+++ SQL/310123.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE TID81D;
+--CREATE DATABASE TID81D;
 USE TID81D;
 
-CREATE TABLE Sexo(
+CREATE TABLE Sexos(
     idSexo INT NOT NULL IDENTITY(1,1),
     descripcion VARCHAR(10),
     PRIMARY KEY (idSexo)
 );
 
-DROP TABLE Cliente;
-CREATE TABLE Cliente(
+--DROP TABLE Cliente;
+CREATE TABLE Clientes(
     idCliente INT NOT NULL IDENTITY(1,1),
     nombre    VARCHAR(50),
     aPaterno  VARCHAR(50),
@@ -18,12 +18,12 @@ CREATE TABLE Cliente(
     sexo      INT,
     telefono  VARCHAR(16),
     PRIMARY KEY (idCliente),
-    CONSTRAINT FK_CLIENTE FOREIGN KEY(sexo) REFERENCES Sexo(idSexo)
+    CONSTRAINT FK_CLIENTE FOREIGN KEY(sexo) REFERENCES Sexos(idSexo)
 );
 
-DROP TABLE Empleado;
+--DROP TABLE Empleado;
 
-CREATE TABLE Empleado(
+CREATE TABLE Empleados(
     idEmpleado INT NOT NULL IDENTITY(1,1),
     nombre    VARCHAR(50),
     aPaterno  VARCHAR(50),
@@ -35,35 +35,69 @@ CREATE TABLE Empleado(
     fechaContrato DATE,
     sueldo DECIMAL(10,2),
     PRIMARY KEY (idEmpleado),
-    CONSTRAINT FK_Empleado FOREIGN KEY(sexo) REFERENCES Sexo(idSexo)
+    CONSTRAINT FK_Sexo FOREIGN KEY(sexo) REFERENCES Sexos(idSexo)
 );
 
-INSERT INTO Sexo (descripcion) VALUES('Femenino');
-INSERT INTO Sexo(descripcion) VALUES ('Maculino');
+INSERT INTO Sexos (descripcion) VALUES('Femenino');
+INSERT INTO Sexos(descripcion) VALUES ('Maculino');
 
-INSERT INTO Cliente(nombre,aPaterno,aMaterno,email,direccion,sexo,telefono) VALUES ('Luis','Loya','Baca','luis.arturo1738@outlook.com','C. Los mayas',2,'614-286-3943');
+INSERT INTO Clientes(nombre,aPaterno,aMaterno,email,direccion,sexo,telefono) VALUES ('Luis','Loya','Baca','luis.arturo1738@outlook.com','C. Los mayas',2,'614-286-3943');
 
-INSERT INTO Cliente(nombre,aPaterno,aMaterno,email,direccion,sexo,telefono) VALUES ('Gabriela','Baca','Maynez','gabrielanov29@hotmail.com','C. Los Olmecas',1,'614-142-3932');
-INSERT INTO Cliente(nombre,aPaterno,aMaterno,email,direccion,sexo,telefono) VALUES ('Fernanda','Loya','Venegas','mafeloya93@outlook.com','Av. INFONAVIT',1,'614-114-5624');
+INSERT INTO Clientes(nombre,aPaterno,aMaterno,email,direccion,sexo,telefono) VALUES ('Gabriela','Baca','Maynez','gabrielanov29@hotmail.com','C. Los Olmecas',1,'614-142-3932');
+INSERT INTO Clientes(nombre,aPaterno,aMaterno,email,direccion,sexo,telefono) VALUES ('Fernanda','Loya','Venegas','mafeloya93@outlook.com','Av. INFONAVIT',1,'614-114-5624');
 
-INSERT INTO Empleado(nombre,aPaterno,aMaterno,email,direccion,sexo,telefono,fechaContrato,sueldo) VALUES ('Mariana','Garcia','Chacon','mgachon@gmail.com','C. Churubascos',2,'614-924-9447','2008-10-17',2640.20);
-INSERT INTO Empleado(nombre,aPaterno,aMaterno,email,direccion,sexo,telefono,fechaContrato,sueldo) VALUES ('Humberto','Jaquez','Villalobos','hjaqVillalobos@gmail.com','C. Taracos',1,'614-742-852-6468','2000-08-02',10000.42);
+INSERT INTO Empleados(nombre,aPaterno,aMaterno,email,direccion,sexo,telefono,fechaContrato,sueldo) VALUES ('Mariana','Garcia','Chacon','mgachon@gmail.com','C. Churubascos',2,'614-924-9447','2008-10-17',2640.20);
+INSERT INTO Empleados(nombre,aPaterno,aMaterno,email,direccion,sexo,telefono,fechaContrato,sueldo) VALUES ('Humberto','Jaquez','Villalobos','hjaqVillalobos@gmail.com','C. Taracos',1,'614-742-852-6468','2000-08-02',10000.42);
 
 
-CREATE TABLE Producto
+CREATE TABLE Productos
 (idProducto int identity(1,1) not null,
 Descripcion  varchar(50),
 Precio    decimal(8,2),
 inventario int,
 Constraint PK_Producto Primary Key(idProducto)
 );
-select * from PRODUCTO;
+select * from Productos;
 
- 
+insert into Productos values('Escoba magica',52.50,25);
+insert into Productos values('Cubeta de 5 ltos',67.25,50);
+insert into Productos values('Cepillo industrial',250.00,20);
 
-insert into PRODUCTO values('Escoba magica',52.50,25);
-insert into PRODUCTO values('Cubeta de 5 ltos',67.25,50);
-insert into PRODUCTO values('Cepillo industrial',250.00,20);
+--DROP table Productos;
 
-DROP table PRODUCTO;
+CREATE TABLE TipoContrato (
+    IdTipoContrato INT NOT NULL IDENTITY(1,1),
+    Descripcion VARCHAR(50),
+    PRIMARY KEY(IdTipoContrato) 
+);
 
+DROP table TipoUsuarios;
+
+CREATE TABLE TipoUsuario (
+    IdTipoUsuario INT NOT NULL IDENTITY(1,1),
+    Descripcion VARCHAR(50),
+    PRIMARY KEY(IdTipoUsuario) 
+);
+
+ALTER Table Empleados
+ADD TipoContrato INT FOREIGN KEY(TipoContrato) REFERENCES TipoContrato(IdTipoContrato);
+
+ALTER Table Empleados
+ADD TipoUsuario INT FOREIGN KEY(TipoUsuario) REFERENCES TipoUsuario(IdTipoUsuario);
+
+--DROP database TID81D;
+
+drop table Clientes, Empleados, Producto, Sexo, TipoContrato, TipoUsuario, Sexo;
+
+--use master;
+
+
+INSERT INTO TipoContrato VALUES('Por horas');
+INSERT INTO TipoContrato VALUES('Tiempo Completo');
+INSERT INTO TipoContrato VALUES ('Indeterminado');
+
+INSERT INTO TipoUsuario VALUES ('Administrador');
+INSERT INTO TipoUsuario(Descripcion) VALUES ('Tecnico');
+INSERT INTO TipoUsuario VALUES ('Vendedor');
+
+UPDATE Empleado SET TipoContrato WHERE idEmpleado = 1;
