a0a07fc0cad28274fbbb397e04258a97c298a512
Arturo Loya <41841053+ArturShadow@users.noreply.github.com>
2023-03-07 16:34:48 -0600

    db

--- Tienda/SQL/db_Tienda.sql
+++ Tienda/SQL/db_Tienda.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE Tienda;
+USE Tienda;
 
 CREATE TABLE Clientes (
-    CodClientes INT NOT NULL IDENTITY(1,1),
+    CodCliente INT NOT NULL IDENTITY(1,1),
     Nombre VARCHAR(50),
     Apellidos VARCHAR(100),
     Empresa VARCHAR(100),
@@ -10,14 +11,35 @@ CREATE TABLE Clientes (
     Provincia VARCHAR(60),
     Telefono VARCHAR(15),
     FehaNacimiento Date,
-    CONSTRAINT  PK_Clientes PRIMARY KEY(CodClientes)
+    CONSTRAINT  PK_Clientes PRIMARY KEY(CodCliente)
 );
 
 CREATE TABLE Articulos (
     CodArticulo INT NOT NULL IDENTITY(1,1),
     NombreArticulo VARCHAR(100),
-    
+    Descripcion VARCHAR(250),
+    PrecioUnitario DECIMAL,
+    Stock INT,
+    StockExtra INT,
+    imagen image,
     CONSTRAINT PK_Articulos PRIMARY KEY(CodArticulo)
 );
 
+CREATE TABLE Compras (
+    Cliente INT,
+    Articulo INT,
+    Fecha DATE,
+    Unidades INT,
+    Total DECIMAL
+    CONSTRAINT FK_Cliente FOREIGN KEY(Cliente) REFERENCES Clientes(CodCliente),
+    CONSTRAINT FK_Articulo FOREIGN KEY(Articulo) REFERENCES Articulos(CodArticulo),
+    CONSTRAINT PK_Compras PRIMARY KEY(Cliente, Articulo)
+);
+
+-- Insert rows into table 'Articulos'
+INSERT INTO Articulos ("NombreArticulo", "Descripcion", "PrecioUnitario","Stock", "StockExtra") VALUES ('Tijeras','Cortan papel',2.99,30,8);
+GO
 -- 520-614-256-286
+
+ALTER TABLE Articulos
+DROP COLUMN Imagen;
---
+++ Tienda/add-nugets.bat
@@ -0,0 +1,4 @@
+'
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 8.0.0-preview.1.23111.4
+dotnet add package Microsoft.EntityFrameworkCore.Tools --version 8.0.0-preview.1.23111.4
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 8.0.0-preview.1.23111.4
